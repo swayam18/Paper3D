f65ce90bdc1b46746f40c1c450d72939d9ddf572
Toh Yong Cheng <tohyongcheng@gmail.com>
2015-08-01 23:57:20 +0800

    Added stupid evolution

--- evolution.py
+++ evolution.py
@@ -1,11 +1,11 @@
-from tree import parseArrayIntoTree
+from tree import parseArrayIntoTree, parseEdgeArrayIntoTree
 import random
 
 class TreeWorld:
   def __init__(self, graph, arrays):
     self.graph = graph
     self.arrays = arrays
-    self.mutation_p = 0.1
+    self.mutation_p = 1.0
 
   def evaluate(self):
     fitness = [0] * len(self.arrays)
@@ -55,10 +55,10 @@ class TreeWorld:
     parent1 = self.arrays[i][:]
     parent2 = self.arrays[j][:]
 
-    (child1, child2) = self.crossOver(parent1,parent2)
-    self.mutate(child1)
-    self.mutate(child2)
-    return [child1, child2]
+    # (child1, child2) = self.crossOver(parent1,parent2)
+    self.mutate(parent1)
+    self.mutate(parent2)
+    return [parent1, parent2]
 
   def crossOver(self,a, b):
     i = min(a.index(-1), b.index(-1))
@@ -74,10 +74,10 @@ class TreeWorld:
     # see if we need to mutate
     roll = random.random()
     if roll > self.mutation_p:
-      return
+      return list_of_edges
     i = random.randint(0,len(list_of_edges)-1)
     if list_of_edges[i] == -1:
-      return
+      return list_of_edges
     
     # choose random node, and a random child
     random_node = self.graph.nodes[i]
@@ -90,16 +90,21 @@ class TreeWorld:
       i = temp
 
     if (i, j) in list_of_edges:
-      return
+      return list_of_edges
     else:
+      print "Mutating!"
       # remove a random edge that was in the cycle.
       r = random.choice([i,j])
-      l = [edge for edge in list_of_edges if r in edge]
+
+      l = [edge for edge in list_of_edges if (r in edge)]
       rm = random.choice(l)
       list_of_edges.remove(rm)
-
       # insert in list_of_edges
       list_of_edges.append((i,j))
+      print r
+      print list_of_edges
+    return list_of_edges
+
 
   def selectRandom(self,fitness):
     total = sum(fitness)
@@ -112,13 +117,6 @@ class TreeWorld:
   # fitness is the number of non intersecting triangles
   def evaluate_array(self, array):
     # consistency check
-    for i,j in enumerate(array):
-      # if the triangles aren't neighbours, discard this mutant
-      if j != -1:
-        node_i = self.graph.nodes[i]
-        node_j = self.graph.nodes[j]
-        if self.graph.connected(node_i,node_j) == -1:
-          return 0
     tn = parseArrayIntoTree(self.graph.nodes, array)
     tn.unfold()
     intersects = tn.checkIntersection()
